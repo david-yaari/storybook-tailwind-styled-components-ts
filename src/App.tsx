6584edc10635c92fc8384e963053a74038ede2aa
David Yaari <david.yaari@icloud.com>
2020-11-22 19:50:49 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='text-center'>
+      <header className='bg-gray-900 min-h-screen flex flex-col items-center justify-center text-4xl text-white'>
+        <img
+          src={logo}
+          className='App-logo h-64 pointer-events-none'
+          alt='logo'
+        />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='text-blue-300'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
